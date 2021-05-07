24ff804a96ebd410bf1d72fcfd7306cc7d331b4e
izzydoesit <15269860+izzydoesit@users.noreply.github.com>
2021-05-06 21:10:59 -0400

    Fix color attribute to compare to pickedColor

--- colorGame.js
+++ colorGame.js
@@ -31,7 +31,7 @@ function setupModeButtons(){
 function setupSquares(){
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].addEventListener("click", function() {
-			var clickedColor = this.style.background;
+			var clickedColor = this.style.backgroundColor;
 			if (clickedColor === pickedColor) {
 				messageDisplay.textContent = "Correct!";
 				resetButton.textContent = "Play Again?";
